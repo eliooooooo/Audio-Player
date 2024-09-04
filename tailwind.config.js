2ac39bbdb2daf3a44d96a9dc9c979df5acf6d288
Eliott <eliott.burkle@gmail.com>
2024-09-04 09:43:24 +0200

    Feat: list Integration

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./public/*.{html,js}"],
+    theme: {
+        extend: {},
+        screens: {
+            'xs': '320px',
+            'sm': '480px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1440px',
+        },
+    },
+    plugins: [],
+}
